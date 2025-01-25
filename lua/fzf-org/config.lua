536269fd88d8688cc262ad2edf378f198f95d409
0xzhzh <178814591+0xzhzh@users.noreply.github.com>
2025-01-24 23:28:33 -0800

    doc: adjust formatting for type annotations

--- lua/fzf-org/config.lua
+++ lua/fzf-org/config.lua
@@ -5,34 +5,38 @@ local links = require("fzf-org.links")
 local actions = require("fzf-org.actions")
 
 ---@class fzo.Opts
----@field where         fzo.Where|fzo.Item[]|nil      where we should search (default: "*")
----@field what          "headline"|"file"|nil         what we are searching for (default: "headline")
----@field whence        "headline"|"file"|false|nil   where we should have started from (default: false)
----@field color_icons   boolean|nil                   colorize entries (default: true)
----@field bullet_icons  fzo.BulletIcons|nil           how to display bullets (default: icons)
----@field todo_icons    fzo.TodoIcons|nil             how to display TODO (default: icons)
----@field show_tags     boolean|nil                   whether to display tags
----@field _origin       fzo.Link|false|nil            where we started from
+---@field where   fzo.Where|fzo.Item[]|nil        where we should search (default: "*")
+---@field what    '"headline"'|'"file"'|nil       what we are searching for (default: "headline")
+---@field whence  '"headline"'|'"file"'|false|nil where we should have started from (default: false)
+---@field _origin fzo.Link|false|nil              where we started from
+---
+---@field color_icons   boolean|nil           colorize entries (default: true)
+---@field bullet_icons  fzo.BulletIcons|nil   how to display bullets (default: icons)
+---@field todo_icons    fzo.TodoIcons|nil     how to display TODO (default: icons)
+---@field show_tags     boolean|nil           whether to display tags
 ---
 ---@field previewer string|table|nil
----@field fzf_opts  table<string, string|boolean>|nil
+---@field fzf_opts  table<string,string|boolean>|nil
 ---@field prompt    string|nil
----@field actions   table<string, table|function>|nil
+---@field actions   table<string,table|function>|nil
 ---@field headers   string[]|nil
 
+--- Where to look for Org items
 ---@alias fzo.Where
----| "*" # all org files
----| "%" # current org file (implies whence = "file")
----| "." # current org headline (implies whence = "headline")
+---| '"*"' # all org files
+---| '"%"' # current org file (implies whence = "file")
+---| '"."' # current org headline (implies whence = "headline")
 
+--- How to display bullets
 ---@alias fzo.BulletIcons
 ---| string[] # custom icons (e.g., { "*", "**", "***" })
----| false    # don't show bullets
+---| false    # do not show bullets
 
+--- How to display TODO
 ---@alias fzo.TodoIcons
 ---| { TODO: string, DONE: string, default: string|nil }  # value/type to symbol mapping
----| "value"  # just show the TODO value
----| false    # don't show TODO symbols
+---| '"value"'  # just show the TODO value
+---| false      # don't show TODO symbols
 
 ---@type fzo.BulletIcons
 M.bullet_icons = { "◉", "○", "✸", "✿" }
