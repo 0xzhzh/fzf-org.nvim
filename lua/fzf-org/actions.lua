70c57d54010f5781f7005298e88b7e68cf250bef
0xzhzh <178814591+0xzhzh@users.noreply.github.com>
2025-01-27 23:48:35 -0800

    fix(refile): refile entire file if in capture buffer
    Fixes #3
    
    There is currently no way to tell whether we are in a capture buffer
    other than by looking at the filename, so we pretend we are about to
    create a tempfile and then check whether it is in the same dir as the
    source file, to determine if it is in a
    
    See also: https://github.com/nvim-orgmode/orgmode/issues/872

--- lua/fzf-org/actions.lua
+++ lua/fzf-org/actions.lua
@@ -6,20 +6,56 @@ local org = require("fzf-org.org")
 local utils = require("fzf-org.utils")
 local links = require("fzf-org.links")
 
+---@param p string
+---@return string dir
+local function dirname(p)
+  if not p then return "" end
+  return vim.uv.fs_realpath(vim.fs.dirname(p)) or ""
+end
+
+--- HACK: check if we are refiling a capture, which will be in a temporary file
+--- See: https://github.com/nvim-orgmode/orgmode/issues/872
+---@param headline fzo.Headline
+---@return boolean
+local function is_capture(headline)
+  return dirname(headline.file.filename) == dirname(vim.fn.tempname())
+end
+
 ---@param selected string[]
 ---@param opts fzo.Opts
 function M.refile_headline(selected, opts)
   local src_kind, src = links.resolve_item(opts._origin)
   local dst_kind, dst = links.resolve_item(selected[1] --[[@as fzo.Link]])
-  if src_kind == "headline" and dst then
-    org.refile({ source = src --[[@as fzo.Headline]], destination = dst })
-    if dst_kind == "headline" then
-      ---@cast dst fzo.Headline
-      utils.info(string.format("refiled under %s", dst.title))
-    elseif dst_kind == "file" then
-      ---@cast dst fzo.File
-      utils.info(string.format("refiled to %s", dst.filename))
+  if src_kind ~= "headline" then
+    utils.err("could not refile: no headline found at cursor")
+    return
+  end
+  if not dst then
+    utils.err("could not refile: no destination found")
+    return
+  end
+
+  ---@cast src fzo.Headline
+  ---@type string
+  local src_description = src_kind
+
+  if is_capture(src) then
+    local parent = src.parent
+    while parent do
+      src = parent
+      parent = src.parent
     end
+    src_description = "capture"
+  end
+
+  org.refile({ source = src, destination = dst })
+
+  if dst_kind == "headline" then
+    ---@cast dst fzo.Headline
+    utils.info(string.format("refiled %s under %s", src_description, dst.title))
+  elseif dst_kind == "file" then
+    ---@cast dst fzo.File
+    utils.info(string.format("refiled %s to %s", src_description, dst.filename))
   end
 end
 
